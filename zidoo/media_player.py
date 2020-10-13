4d1fb38d433a166b500fe028768e72e009827d24
wizmo2 <wizmo.home@yahoo.com>
2020-10-13 16:53:43 -0400

    Update to MediaPlayerEntity

--- zidoo/media_player.py
+++ zidoo/media_player.py
@@ -1,4 +1,4 @@
-"""Support for interface with Zido Media Player."""
+"""Support for interface with Zidoo Media Player."""
 import ipaddress
 import logging
 
@@ -6,16 +6,16 @@ from .zidoorc import ZidooRC
 from getmac import get_mac_address
 import voluptuous as vol
 
-from homeassistant.components.media_player import PLATFORM_SCHEMA, MediaPlayerDevice
+from homeassistant.components.media_player import PLATFORM_SCHEMA, MediaPlayerEntity
 from homeassistant.components.media_player.const import (
     SUPPORT_CLEAR_PLAYLIST,
     SUPPORT_NEXT_TRACK,
     SUPPORT_PAUSE,
     SUPPORT_PLAY,
-    SUPPORT_PLAY_MEDIA,	
-    SUPPORT_PREVIOUS_TRACK,	
+    SUPPORT_PLAY_MEDIA,
+    SUPPORT_PREVIOUS_TRACK,
     SUPPORT_SEEK,
-    SUPPORT_SELECT_SOURCE,	
+    SUPPORT_SELECT_SOURCE,
     SUPPORT_SELECT_SOUND_MODE,
     SUPPORT_SHUFFLE_SET,
     SUPPORT_STOP,
@@ -36,11 +36,20 @@ from homeassistant.components.media_player.const import (
     MEDIA_TYPE_GAME,
     MEDIA_TYPE_APP,
 )
-from homeassistant.const import CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_NAME,
+    STATE_IDLE,
+    STATE_OFF,
+    STATE_PAUSED,
+    STATE_PLAYING,
+)
+
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util.json import load_json, save_json
+from homeassistant.util.dt import utcnow
 
-DEFAULT_NAME = 'Zidoo MediaPlayer'
+DEFAULT_NAME = "Zidoo MediaPlayer"
 
 # Map ip to request id for configuring
 _LOGGER = logging.getLogger(__name__)
@@ -53,28 +62,33 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 CLIENTID_PREFIX = "HomeAssistant"
-DEFAULT_NAME = "Zidoo Player"
 NICKNAME = "Home Assistant"
+ZIDOO_VIDEOPLAYER = "Media Center"
+ZIDOO_ZIDOOPOSTER = "Home Theater 3.0"
+ZIDOO_MUSICPLAYER = "Music Player 5.0"
 
 SUPPORT_ZIDOO = (
-    SUPPORT_NEXT_TRACK
-    | SUPPORT_PAUSE	
-    | SUPPORT_PLAY
-    | SUPPORT_PLAY_MEDIA	
-    | SUPPORT_PREVIOUS_TRACK	
-    | SUPPORT_SELECT_SOURCE	
-    | SUPPORT_STOP	
-    | SUPPORT_TURN_OFF	
-    | SUPPORT_TURN_ON	
-    | SUPPORT_VOLUME_MUTE	
-    | SUPPORT_VOLUME_STEP 
+    SUPPORT_VOLUME_STEP
+    | SUPPORT_VOLUME_MUTE
+    | SUPPORT_TURN_ON
+    | SUPPORT_TURN_OFF
+    | SUPPORT_SELECT_SOURCE
 )
-    #SUPPORT_CLEAR_PLAYLIST
-    #SUPPORT_SEEK	
-    #SUPPORT_SELECT_SOUND_MODE	
-    #SUPPORT_SHUFFLE_SET	
-    #SUPPORT_VOLUME_SET	
-    
+# SUPPORT_CLEAR_PLAYLIST
+# SUPPORT_SEEK
+# SUPPORT_SELECT_SOUND_MODE
+# SUPPORT_SHUFFLE_SET
+# SUPPORT_VOLUME_SET
+
+SUPPORT_MEDIA_MODES = (
+    SUPPORT_PAUSE
+    | SUPPORT_STOP
+    | SUPPORT_PREVIOUS_TRACK
+    | SUPPORT_NEXT_TRACK
+    | SUPPORT_PLAY
+    | SUPPORT_PLAY_MEDIA
+)
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Zidoo platform."""
@@ -84,8 +98,8 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     add_entities([ZidooPlayerDevice(host, name)])
 
 
-class ZidooPlayerDevice(MediaPlayerDevice):
-    """Representation of a Zido Media."""
+class ZidooPlayerDevice(MediaPlayerEntity):
+    """Representation of a Zidoo Media."""
 
     def __init__(self, host, name):
         """Initialize the Zidoo device."""
@@ -94,21 +108,23 @@ class ZidooPlayerDevice(MediaPlayerDevice):
         self._name = name
         self._state = STATE_OFF
         self._muted = False
-        self._program_name = None
-        self._channel_name = None
         self._source = None
         self._source_list = []
         self._original_content_list = []
         self._content_mapping = {}
+        self._title = None
+        self._artist = None
+        self._album = None
         self._duration = None
+        self._position = None
         self._content_uri = None
         self._id = None
         self._playing = False
-        self._start_date_time = None
         self._program_media_type = None
         self._min_volume = None
         self._max_volume = None
         self._volume = None
+        self._last_update = None
 
         self._zidoorc.connect(CLIENTID_PREFIX, NICKNAME)
         if self._zidoorc.is_connected():
@@ -126,48 +142,52 @@ class ZidooPlayerDevice(MediaPlayerDevice):
 
         # Retrieve the latest data.
         try:
-            if self._state == STATE_ON:
-                # self._refresh_volume()
-                self._refresh_channels()
-            
             power_status = self._zidoorc.get_power_status()
             if power_status == "on":
-                self._state = STATE_ON
+                self._state = STATE_PAUSED
                 playing_info = self._zidoorc.get_playing_info()
                 self._reset_playing_info()
                 if playing_info is None or not playing_info:
                     self._channel_name = "Standby"
                     self._program_media_type = MEDIA_TYPE_APP
                 else:
-                    self._program_name = playing_info.get("title")
-                    self._channel_name = playing_info.get("artist")
+                    self._title = playing_info.get("title")
+                    self._artist = playing_info.get("artist")
                     mediatype = playing_info.get("source")
-                    #_LOGGER.info("media-type" + str(mediatype))
                     if mediatype and mediatype is not None:
-                        if mediatype == 'video':
+                        if mediatype == "video":
                             self._program_media_type = MEDIA_TYPE_VIDEO
-                        else: 
+                            self._source = ZIDOO_VIDEOPLAYER
+                        else:
                             self._program_media_type = MEDIA_TYPE_MUSIC
+                            self._source = ZIDOO_MUSICPLAYER
                     else:
                         self._program_media_type = MEDIA_TYPE_APP
-                    #self._source = playing_info.get("source")
-                    self._content_uri = playing_info.get("uri")
+                    status = playing_info.get("status")
+                    if status and status is not None:
+                        if status == 1 or status == True:
+                            self._state = STATE_PLAYING
                     self._duration = playing_info.get("duration")
-                    #self._start_date_time = playing_info.get("startDateTime")
+                    self._position = playing_info.get("position")
+                    self._content_uri = playing_info.get("uri")
+                    self._last_update = utcnow()
+                self._refresh_channels()
             else:
                 self._state = STATE_OFF
 
         except Exception as exception_instance:  # pylint: disable=broad-except
             _LOGGER.error(exception_instance)
-            #self._state = STATE_OFF
+            # self._state = STATE_OFF
 
     def _reset_playing_info(self):
-        self._program_name = None
-        self._channel_name = None
+        self._title = None
+        self._artist = None
+        self._album = None
         self._program_media_type = None
         self._source = None
         self._content_uri = None
         self._duration = None
+        self._position = None
         self._start_date_time = None
 
     def _refresh_volume(self):
@@ -210,9 +230,9 @@ class ZidooPlayerDevice(MediaPlayerDevice):
     def media_content_type(self):
         """Content type of current playing media."""
         self._program_media_type
-        
-    #@property
-    #def volume_level(self):
+
+    # @property
+    # def volume_level(self):
     #    """Volume level of the media player (0..1)."""
     #    if self._volume is not None:
     #        return self._volume / 100
@@ -226,17 +246,25 @@ class ZidooPlayerDevice(MediaPlayerDevice):
     @property
     def supported_features(self):
         """Flag media player features that are supported."""
-        return SUPPORT_ZIDOO
+        return SUPPORT_ZIDOO | SUPPORT_MEDIA_MODES
 
     @property
     def media_title(self):
         """Title of current playing media."""
-        return_value = None
-        if self._program_name is not None:
-            return_value = self._program_name
-            if self._channel_name is not None:
-                return_value = self._channel_name + " : " + return_value
-        return return_value
+        title = self._title
+        if self._artist is not None:
+            title = self._artist + " : " + title
+        return title
+
+    @property
+    def media_artist(self):
+        """Artist of current playing media."""
+        return self._artist
+
+    @property
+    def media_album_name(self):
+        """Album of current playing media."""
+        return self._album
 
     @property
     def media_content_id(self):
@@ -248,7 +276,17 @@ class ZidooPlayerDevice(MediaPlayerDevice):
         """Duration of current playing media in seconds."""
         return self._duration
 
-    #def set_volume_level(self, volume):
+    @property
+    def media_position(self):
+        """Position of current playing media in seconds."""
+        return self._position
+
+    @property
+    def media_position_updated_at(self):
+        """Last time status was updated."""
+        return self._last_update
+
+    # def set_volume_level(self, volume):
     #    """Set volume level, range 0..1."""
     #    self._zidoorc.set_volume_level(volume)
 
@@ -275,8 +313,9 @@ class ZidooPlayerDevice(MediaPlayerDevice):
     def select_source(self, source):
         """Set the input source."""
         if source in self._content_mapping:
-            uri = self._content_mapping[source]
-            self._zidoorc.play_content(uri)
+            self._zidoorc.start_app(source)
+            # uri = self._content_mapping[source]
+            # play_content(uri)
 
     def media_play_pause(self):
         """Simulate play pause media player."""
