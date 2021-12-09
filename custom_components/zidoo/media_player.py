690db6575af56407bbc3c30a5fa944f04cae82cc
Wizmo2 <wizmo.homne@yahoo.com>
2021-12-09 05:50:12 -0500

    format chnage

--- custom_components/zidoo/media_player.py
+++ custom_components/zidoo/media_player.py
@@ -38,7 +38,14 @@ from homeassistant.const import (
     STATE_PAUSED,
     STATE_PLAYING,
 )
-from .const import DOMAIN, _LOGGER, CLIENTID_PREFIX, CLIENTID_NICKNAME, CONF_SHORTCUT, ZTYPE_MEDIA_TYPE
+from .const import (
+    DOMAIN,
+    _LOGGER,
+    CLIENTID_PREFIX,
+    CLIENTID_NICKNAME,
+    CONF_SHORTCUT,
+    ZTYPE_MEDIA_TYPE,
+)
 
 import homeassistant.helpers.config_validation as cv
 
@@ -133,10 +140,10 @@ class ZidooPlayerDevice(MediaPlayerEntity):
         self._volume = None
         self._last_update = None
 
-        #response = self._player.connect(CLIENTID_PREFIX, CLIENTID_NICKNAME)
-        #if response is not None:
+        # response = self._player.connect(CLIENTID_PREFIX, CLIENTID_NICKNAME)
+        # if response is not None:
         #    self.update()
-        #else:
+        # else:
         #    self._state = STATE_OFF
 
     def update(self):
